c0fe17d02cc3b44b0dc4e5de78fe1353a464e783
shivangi <shivangi.jain@tothenew.com>
2019-09-27 17:26:38 +0530

    Question 3Given two lists [11, 12, 13, 14] and [13, 14, 15], how would we obtain the list of items from the first that are not in the second?

---
+++ src/Question3.groovy
@@ -0,0 +1,9 @@
+class Question3 {
+    static void main(String[] args) {
+        List list1 = [11, 12, 13, 14]
+        List list2 = [13, 14, 15]
+
+        println"Output::: "+(list1 - list2)
+
+    }
+}
