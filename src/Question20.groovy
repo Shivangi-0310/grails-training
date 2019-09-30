82da86cabada1da2e1215be84460823dd11d543c
shivangi <shivangi.jain@tothenew.com>
2019-09-30 06:48:02 +0530

    Question 20: Find if a map contains a particular key.

---
+++ src/Question20.groovy
@@ -0,0 +1,9 @@
+class Question20 {
+    static void main(String[] args) {
+        Map<String,Integer> map = ["a": 1, "b": 2, "c": 3, "d": 4, "e": 5]
+        println(map.containsKey("a"))
+        println(map.containsKey("j"))
+
+
+    }
+}
