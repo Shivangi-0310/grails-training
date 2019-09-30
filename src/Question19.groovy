ae39bdf54093a0d47cf4cc1620bd4e97de83fc3d
shivangi <shivangi.jain@tothenew.com>
2019-09-30 06:47:46 +0530

    Question 19: Consider the following map: Map m = ['1' : 2, '2' : 3, '3' : 4, '2':5] Is this a valid construction? What is the value of m['2']?

---
+++ src/Question19.groovy
@@ -0,0 +1,10 @@
+class Question19 {
+    static void main(String[] args) {
+        Map m = ['1': 2, '2': 3, '3': 4, '2': 5]
+        println(m)
+
+        println("Yes Valid ")
+        println(m['2'])
+
+    }
+}
