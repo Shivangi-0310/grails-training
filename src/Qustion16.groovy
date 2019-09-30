0b8685ef1fdce8121a5973efac4d344c0689944e
shivangi <shivangi.jain@tothenew.com>
2019-09-30 06:47:05 +0530

    Question 16: Create a new map consisting of 10 of your friend's name's as keys and their ages as value.

---
+++ src/Qustion16.groovy
@@ -0,0 +1,5 @@
+class Qustion16 {
+    static void main(String[] args) {
+        Map nameAndAge = ["aakash":24, "karishma":23, "rajat":24, "akshita":21, "kamal":24, "ashu":22, "babloo":22, "joyesh":24, "mudit":23, "ankit":27]
+    }
+}
