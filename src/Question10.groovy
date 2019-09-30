106deb648183c24dda330edb7c00e7f6a9dbcecf
shivangi <shivangi.jain@tothenew.com>
2019-09-30 06:44:50 +0530

    Question 10: Get first, second and last element of Range.

---
+++ src/Question10.groovy
@@ -0,0 +1,8 @@
+class Question10 {
+    static void main(String... s){
+        List list= 1..10
+        println "First element :: ${list.first()}"
+        println "Second element :: ${list.get(1)}"
+        println "Last element :: ${list.last()}"
+    }
+}
